dcac235644d5ecd39334256a628e97fab6d22e4d
krikaz <pascal.bou@protonmail.com>
2019-07-17 16:35:39 +0200

    created addStep method

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -21,6 +21,12 @@ function findById(id) {
 		.first();
 }
 
+function findStepById(id) {
+	return db('steps')
+		.where({ id })
+		.first();
+}
+
 function findSteps(id) {
 	return db
 		.select('steps.id', 'step_number', 'instructions', 'scheme_name')
@@ -34,7 +40,10 @@ async function add(scheme) {
 	return findById(id);
 }
 
-function addStep() {}
+async function addStep(step, scheme_id) {
+  const [step_id] = await db('steps').insert({...step, scheme_id: scheme_id});
+  return findStepById(step_id);
+}
 
 async function update(changes, id) {
 	await findById(id).update(changes);
